5f77190230470f8b09cd05051e9572a38a7e68bc
jcasado <1eselnumeroXD>
2021-03-11 17:53:33 +0100

    Cambios

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
+// Jacobo Casado de Gracia. Práctica 1 de Metaheurística. Greedy y Búsqueda Local.
+
+/* En primer lugar, la práctica está dividida de estas maneras:
+ * 1: Almacenamiento de datos para un mejor uso en los algoritmos.
+ * 2: Aplicación del algoritmo Greedy y aplicación del algoritmo BL.
+ * 3: Representación y guardado de datos.
+ */
+
 #include <iostream>
+
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
