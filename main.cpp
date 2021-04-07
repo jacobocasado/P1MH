d2ba28d08ca658787ecec2f0489416b545fcf15c
jcasado <casacojacobo@gmail.com>
2021-04-07 17:44:10 +0200

    Greedy que parece ser el optimo.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 // Jacobo Casado de Gracia. Práctica 1 de Metaheurística. Greedy y Búsqueda Local.
 
 /* En primer lugar, la práctica está dividida de estas maneras:
- * 1: Almacenamiento de datos para un mejor uso en los algoritmos.
- * 2: Aplicación del algoritmo Greedy y aplicación del algoritmo BL.
- * 3: Representación y guardado de datos.
+ *
  */
 
 using namespace std;
